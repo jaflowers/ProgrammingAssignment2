3f9fba9b111150cdf81fa2205c7dfc4691f637e8
jaflowers <jessicaflowers11@yahoo.com>
2015-03-21 13:11:17 -0700

    J.Walston's Assignment 2
    Cache a matrix and its inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+#############################GOAL###############################
+# create a place to store matrix (Cache) and its inverse
+# Create a function that inverses the stored matrix, unless it is inversed
+# Retrieve already inversed matrix from cache (!is.null(of inverse?))
+# (s)set matrix, (g)get matrix, (si)set inverse matrix, (gi) get inverse matrix
+# store above in object to use in cache solve function
+# Return/print inversed matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+#######################CacheMatrix##############################
+##creates a place to store matrix and its inverse
+makeCacheMatrix <- function(x = matrix()) { #Square matrix=x  
+  inm = NULL #default Cache
+  s = function(y) {  #default cache
+    x <<- y #Cache Matrix
+    inm <<- NULL #Empty cache
+  }
+  g = function() x
+  si = function(inverse) inm <<- inverse 
+  gi = function() inm
+  list(s=s, g=g, si=si, gi=gi)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+########################CacheSolve##############################
+##Caches matrix if not inversed or recalls already inversed matric from cache
+cacheSolve <- function(x, ...) { 
+  inm = x$gi()
+  if (!is.null(inm)){
+    message("retrieve cached matrix")
+    return(inm)
+  }
+  mcache = x$g() #calculate inverse if no cached matrix
+  inm = solve(mcache, ...)
+  x$si(inm) #cache inverse
+  return(inm) #print inverse matirx
+  #invisible(inm) #does not print...how to make this work?? 
 }
+
+#############################TEST##############################
+mat <- matrix(data = c(2,4,6,8), nrow = 2, ncol = 2)
+mat2 <- makeCacheMatrix(mat)
+cacheSolve(mat2)
+
+r = rnorm(100)
+mat1 = matrix(r, nrow=10, ncol=10)
+mat3 <- makeCacheMatrix(mat1)
+cacheSolve(mat3)
